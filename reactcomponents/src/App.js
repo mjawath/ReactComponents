8e919dbd2fe62a8b5843dfe8dc58e00e4489a34b
jawath <mohamed.jawath@pearson.com>
2017-10-20 12:11:49 +0530

    componet passing

--- reactcomponents/src/App.js
+++ reactcomponents/src/App.js
@@ -1,44 +1,25 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Items} from './component/MockData'
-import Container from './component/Templatable'
-import List from './component/List'
+import DataGrid from './component/DataGrid'
+
 
 
 class App extends Component {
     render() {
+        const test = (data)=>{
+            return <span>my tag cool {data}</span>;
+        };
         return (
             <div className="App">
                 <div className="App-header">
                     <img src={logo} className="App-logo" alt="logo"/>
                     <h2>Welcome to ReactComponents</h2>
+
                 </div>
                 <div className="App-intro">
-                    This is a demo page for testing the components
-                    <Container data={"my test data"}>
-                        {
-                            (data) =>{
-                                <span>{data}</span>
-                            }
 
-                        }
-                    </Container>
-                </div>
-
-                <div><span>Testing list component</span></div>
-                <div className="App-intro">
-                    This is a List component
-                    <List collection={Items}>
-                        {
-                            (index,itemdata) =>
-                                <div>
-                                    <span>{index}</span>
-                                    <span>{itemdata.desc}</span>
-                                    <span>{itemdata.name}</span>
-                                </div>
-                        }
-                    </List>
+                    <DataGrid contentRender={test}></DataGrid>
                 </div>
             </div>
         );
--- reactcomponents/src/component/test.js
+++ reactcomponents/src/component/test.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {PRODUCTS} from "./MockData";
 
 
 class Mylist extends Component {
